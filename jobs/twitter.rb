f1cf0370e23ddef8154920549233bc772e7eec59
System Administrator <root@mwwes22830.london.net-a-porter.com net-a-porter.com newyork.net-a-porter.com corp.net-a-porter.com>
2015-03-19 16:13:26 +0000

    configured twitter widget

--- jobs/twitter.rb
+++ jobs/twitter.rb
@@ -7,12 +7,12 @@ twitter = Twitter::REST::Client.new do |config|
   config.consumer_key = 'JiWURDCpVYMkMdEh4RZGIdRua'
   config.consumer_secret = 'DtspDRqz2nvNbIv0tNYK9fPH6nEJ0HzxUdd4bXkoX31yTPusqC'
   config.access_token = '2830097575-ZMXbUfcueQjin0WPvzLdj6e79T0ygUmHmVX7CbH'
-  config.access_token_secret = '2830097575-ZMXbUfcueQjin0WPvzLdj6e79T0ygUmHmVX7CbH'
+  config.access_token_secret = 'h5NrVHTK3n9wZRoeyppuJPtaB1ejc3Kg7keTol1b30oW6'
 end
 
-search_term = URI::encode('#thenetbook')
+search_term = URI::encode('@thenetbook')
 
-SCHEDULER.every '10s', :first_in => 0 do |job|
+SCHEDULER.every '20s', :first_in => 0 do |job|
   begin
     tweets = twitter.search("#{search_term}")
 
@@ -26,4 +26,4 @@ SCHEDULER.every '10s', :first_in => 0 do |job|
     puts "Twitter Error: #{e}"
     puts "\e[33mFor the twitter widget to work, you need to put in your twitter API keys in the jobs/twitter.rb file.\e[0m"
   end
-end
+end 
